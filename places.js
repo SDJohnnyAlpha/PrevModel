f7d513a7043219e7da35a5f4659e8814a4d38f68
Stephen Barnes <46560824+SDJohnnyAlpha@users.noreply.github.com>
2021-03-11 14:10:33 +0000

    Update places.js

--- places.js
+++ places.js
@@ -97,7 +97,7 @@ function renderPlaces(places) {
         icon.setAttribute('src', 'RedGeomarker.png');
 
         // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
-        icon.setAttribute('scale', '20, 20');
+        icon.setAttribute('scale', '5, 5');
 
         icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
